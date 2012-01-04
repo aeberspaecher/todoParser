b2decd49ef536fbd5eaa77a9e11d8d12a08b9cf2
Alexander Eberspächer <alex.eberspaecher@googlemail.com>
2012-01-05 00:04:11 +0100

    Initial commit.

---
+++ README.rst
@@ -0,0 +1,41 @@
+==========
+todoParser
+==========
+
+About
+=====
+
+This small script may extract lines that contain statements like "TODO" or
+"FIXME" in comments inside a source file.
+
+todoParser is written in Python and is easily extendable.
+
+Features
+========
+
+Filetypes supported:
+
+- Python/Cython
+- free-form Fortran
+- LaTeX
+
+Usage
+=====
+
+TODO
+
+Known annoyances
+================
+
+The parser will ignore TODOs inside strings. This may be undesired behaviour
+in eg. Python docstrings.
+
+License
+=======
+
+todoParser is distributed under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2 of the
+License, or (at your option) any later version.  A copy of this license can
+be found in the file COPYING included with the source code of this program.
+
+2012 by Alexander Eberspächer
---
+++ parseTodo.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#  Copyright 2012 Alexander Eberspächer <alex(dot)eberspaecher(at)googlemail.com>
+# 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+"""A little script to parse TODO-like statements in source files.
+
+The script parses all (supported) source files in a given path, tries to read
+"TODO: do this and that"-like comments and writes these commands to the
+terminal.
+
+Source formats supported so far:
+
+*.f90: free-form Fortran 90
+*.py: Python
+*.pyx: Cython
+*.tex: LaTeX
+"""
+
+statements = ["TODO", "FIXME"]
+filetypes = [ {"endings": (".f90"), "commentChars": ("!"), "type": "free-form Fortran"},
+              {"endings": (".tex"), "commentChars": ("%"), "type": "LaTeX"},
+              {"endings": (".py", ".pyx"), "commentChars": ("#"), "type": "Python/Cython"}
+            ]
+
+
+
+def findComments(fileName):
+    """Find comments in file 'fileName' and write them to the terminal.
+    """
+
+    # try to open the file:
+    try:
+        fileObj = open(fileName, mode="r")
+    except:
+        print("Could not open file %s."%fileName)
+        return
+
+    # determine filetype by ending:
+    foundType = False
+    for fTypeIndex in range(len(filetypes)): # iterate till ending found
+        if(fileName.endswith(filetypes[fTypeIndex]["endings"])):
+            print("File %s is of type %s."%(fileName, filetypes[fTypeIndex]["type"]))
+            exit
+        if(fTypeIndex == len(filetypes)):
+            print("File %s is not of known type."%fileName)
+            return
+
+    lines = fileObj.readlines()
+
+    # find comments:
+    for i in range(len(lines)):
+        # check if line contains a comment and TODO-like thing
+        for commentStatement in statements:
+            if(commentStatement in lines[i]):
+                # check if the comment statements appears *after* a comment character:
+                for commentChar in filetypes[fTypeIndex]["commentChars"]:
+                    if((lines[i].rfind(commentStatement) > lines[i].rfind(commentChar)) and (lines[i].rfind(commentChar) >= 0)):
+                        print("* %s, line %s: %s"%(fileName, i+1, lines[i]))
+
+                # TODO: possibly include multi-line TODOs as well
+                
+findComments("parseTodo.py")
