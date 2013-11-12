7101d70a0d8953229a573a7e4d2d58844866e90a
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2013-11-12 18:50:50 +0100

    Refactoring
    - change names to lowercase
    - use tuples instead of lists

--- parse_todos.py
+++ parse_todos.py
@@ -37,65 +37,65 @@ Source formats supported so far:
 import os
 from optparse import OptionParser
 
-statements = ["TODO", "FIXME"]
-filetypes = [ {"endings": (".f90"), "commentChars": ("!"), "type": "free-form Fortran"},
-              {"endings": (".tex"), "commentChars": ("%"), "type": "LaTeX"},
-              {"endings": (".py", ".pyx"), "commentChars": ("#"), "type": "Python/Cython"},
-              {"endings": (".c", ".h", ".cpp", ".hpp"), "commentChars": ("//", "/*"), "type": "C/C++"}
-            ]
-whitespaceChars = [ " ", "\t"]
+statements = ("TODO", "FIXME")
+filetypes = ( {"endings": (".f90"), "comment_chars": ("!"), "type": "free-form Fortran"},
+              {"endings": (".tex"), "comment_chars": ("%"), "type": "LaTeX"},
+              {"endings": (".py", ".pyx"), "comment_chars": ("#"), "type": "Python/Cython"},
+              {"endings": (".c", ".h", ".cpp", ".hpp"), "comment_chars": ("//", "/*"), "type": "C/C++"}
+            )
+whitespace_chars = ( " ", "\t")
 
-_linesChecked = 0
-_todoLines = 0
+_lines_checked = 0
+_todo_lines = 0
 
 
-def findTODOs(fileName):
-    """Find comments in file 'fileName' and write them to the terminal.
+def find_todos(filename):
+    """Find comments in file 'filename' and write them to the terminal.
     """
 
-    numOfTODOs = 0
-    global _linesChecked
+    num_todos = 0
+    global _lines_checked
 
     # try to open the file:
     try:
-        fileObj = open(fileName, mode="r")
+        file_obj = open(filename, mode="r")
     except:
-        print("Could not open file %s."%fileName)
-        return numOfTODOs
+        print("Could not open file %s."%filename)
+        return num_todos
 
-    #print "openend %s"%fileName
+    #print "openend %s"%filename
 
     # determine filetype by ending:
-    foundType = False
+    found_type = False
     for i in range(len(filetypes)): # iterate till ending is found
-        if(fileName.endswith(filetypes[i]["endings"])):
-            #print("File %s is of type %s."%(fileName, filetypes[i]["type"]))
-            fTypeIndex = i
-            foundType = True
-    if(not foundType):
+        if(filename.endswith(filetypes[i]["endings"])):
+            #print("File %s is of type %s."%(filename, filetypes[i]["type"]))
+            filetype_index = i
+            found_type = True
+    if(not found_type):
         if(options.verbose):
-            print("File %s is not of known type."%fileName)
-        return numOfTODOs
+            print("File %s is not of known type."%filename)
+        return num_todos
 
-    lines = fileObj.readlines()
-    fileObj.close()
+    lines = file_obj.readlines()
+    file_obj.close()
 
     # find comments:
     for i in range(len(lines)):
-        _linesChecked += 1
+        _lines_checked += 1
         # check if line contains a comment and TODO-like thing
-        for commentStatement in statements:
-            if(commentStatement in lines[i]):
+        for comment_statement in statements:
+            if(comment_statement in lines[i]):
                 # check if the comment statements appears *after* a comment character:
-                for commentChar in filetypes[fTypeIndex]["commentChars"]:
-                    if((lines[i].rfind(commentStatement) > lines[i].rfind(commentChar)) and (lines[i].rfind(commentChar) >= 0)):
-                        printTODOline(fileName, i+1, lines[i])
-                        numOfTODOs += 1
+                for comment_char in filetypes[filetype_index]["comment_chars"]:
+                    if((lines[i].rfind(comment_statement) > lines[i].rfind(comment_char)) and (lines[i].rfind(comment_char) >= 0)):
+                        print_todo_line(filename, i+1, lines[i])
+                        num_todos += 1
 
-    return numOfTODOs
+    return num_todos
 
 
-def printTODOline(fileName, lineNumber, line):
+def print_todo_line(filename, lineNumber, line):
     """Print a line containing a TODO-like statement.
 
     All formatting has to appear here. Leading white space is stripped.
@@ -104,10 +104,10 @@ def printTODOline(fileName, lineNumber, line):
     # find position of first non white-space character:
     i = len(line) - len(line.lstrip())
 
-    print("* %s, line %s: %s"%(fileName, lineNumber, line[i:-1]))  # strip newline
+    print("* %s, line %s: %s"%(filename, lineNumber, line[i:-1]))  # strip newline
 
 
-def listOfVCfiles():
+def vc_files():
     """Return a list of all files that are under version control.
 
     Currently supported: git and svn.
@@ -120,35 +120,36 @@ def listOfVCfiles():
 
     # try git first:
     output = os.popen("git ls-files")
-    outputLines = output.readlines()
-    if(len(outputLines) > 0):
-        if(outputLines[0].rfind(failureStrings["git"]) == -1):  # directory is a git repo:
-            fileList = []
-            for fileName in outputLines:
-                fileList.append(fileName[:-1])  # remove \n
-            return fileList
+    output_lines = output.readlines()
+    if(len(output_lines) > 0):
+        if(output_lines[0].rfind(failureStrings["git"]) == -1):  # directory is a git repo:
+            file_list = []
+            for filename in output_lines:
+                file_list.append(filename[:-1])  # remove \n
+            return file_list
 
     # try svn:
     output = os.popen("svn ls")
-    outputLines = output.readlines()
-    if(len(outputLines) > 0):
-        if(outputLines[0].rfind(failureStrings["git"]) == -1):  # directory is a svn repo:
-            fileList = []
-            for fileName in outputLines:
-                fileList.append(fileName[:-1])  # remove \n
-            return fileList
+    output_lines = output.readlines()
+    if(len(output_lines) > 0):
+        if(output_lines[0].rfind(failureStrings["git"]) == -1):  # directory is a svn repo:
+            file_list = []
+            for filename in output_lines:
+                file_list.append(filename[:-1])  # remove \n
+            return file_list
 
     # TODO: figure out how to use the subprocess module such that STDERR is not shown in terminal
 
     # catch unsuccessful case:
-    print("Directory appears to be neither a git nor svn repository!")
+    print >> sys.err, "Directory appears to be neither a git nor svn repository!"
     return []
+
 
 if(__name__ == "__main__"):
 
     # define options for OptionParser:
-    progUsage = "usage: %prog file1 file2"
-    parser = OptionParser(usage=progUsage)
+    prog_usage = "usage: %prog file1 file2"
+    parser = OptionParser(usage=prog_usage)
     parser.add_option("-c", "--vc", action="store_true", default=False,
                       dest="useVC", help="Use list of files under version control (git/svn).")
     parser.add_option("-v", "--verbose", action="store_true", default=False,
@@ -158,20 +159,20 @@ if(__name__ == "__main__"):
     # decide what to do:
     # if --vc is given, go through the files under version control first:
     if(options.useVC):
-        filesToAppend = listOfVCfiles()
-        if(len(filesToAppend) > 0):
-            for newFile in filesToAppend:
-                args.append(newFile)
+        files = vc_files()
+        if(len(files) > 0):
+            for new_file in files:
+                args.append(new_file)
 
     if(len(args) == 0):
         parser.error("No files specified.")
 
     # go through all files given as an arguments to the script:
-    for fileName in args:
-        number = findTODOs(fileName)
-        _todoLines += number
+    for filename in args:
+        number = find_todos(filename)
+        _todo_lines += number
         if(number > 0):
             print("")  # print a newline after each file processed
 
     print("%s lines checked in total, %s TODO-like statements found."\
-          %(_linesChecked, _todoLines))
+          %(_lines_checked, _todo_lines))
